699ca56e7ff5fc8d7a8c3ef9865a3fe4e7848f7b
Sanjuli Trivedi <sanjuli.trivedi@pharmeasy.in>
2023-12-14 18:29:53 +0530

    updates

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 // src/Home.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React,  { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Home.css'; // Import the CSS file for styling
 
 const Home = () => {
+  const navigate = useNavigate();
+
   const handleScrollToSection = () => {
     const section = document.getElementById('why-generic-section');
     if (section) {
@@ -11,11 +13,38 @@ const Home = () => {
     }
   };
 
+  const allMedicineSuggestions = ['Medicine 1', 'Medicine 2', 'Medicine 3', 'Medicine 4'];
+
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+
+  const handleInputChange = (e) => {
+    const userInput = e.target.value;
+    setSearchInput(userInput);
+
+    // Filter suggestions based on user input
+    const filtered = allMedicineSuggestions.filter((medicine) =>
+      medicine.toLowerCase().includes(userInput.toLowerCase())
+    );
+
+    setFilteredSuggestions(filtered);
+  };
+
+  const handleSearch = () => {
+    // Redirect to the detail page for the selected medicine
+    if (filteredSuggestions.length > 0) {
+      const selectedMedicine = filteredSuggestions[0]; // Assume the first suggestion is selected
+      const medicineId = allMedicineSuggestions.indexOf(selectedMedicine) + 1; // Index starts from 0
+      navigate('/medicine/${medicineId}');
+    }
+  };
+
   return (
     <div className="home-container">
       <div className="home-content">
         <h1>Welcome to the Generic Medicine Store</h1>
-        <p>Find affordable generic medicines for your health needs.</p>
+        <p>Want to save on your monthly expense on medicines?.</p>
+        <h1>Generic is Here!</h1>
         <button className="view-button" onClick={handleScrollToSection}>
           Why Generic Medicine
         </button>
@@ -26,33 +55,44 @@ const Home = () => {
 
       <div className="home-images">
         <img
-          src="https://assets.pharmeasy.in/apothecary/images/medicine_ff.webp?dim=720x0"
+          src={process.env.PUBLIC_URL + '/sample-1 1.png'}
           alt="Medicine Bottle 1"
           className="medicine-image"
         />
-        <img
-          src="https://assets.pharmeasy.in/apothecary/images/health_blogs_ff.webp?dim=720x0"
-          alt="Medicine Bottle 2"
-          className="medicine-image"
-        />
-        <img
-          src="https://assets.pharmeasy.in/apothecary/images/value_store.png?dim=720x0"
-          alt="Medicine Bottle 3"
-          className="medicine-image"
-        />
       </div>
 
+      <p style={{ color: 'black' }}>Compare price of generic alternates of your medicine</p>
       {/* Search box */}
       <div className="search-box">
-        <input type="text" placeholder="Search for medicines..." className="search-input" />
-        <button className="search-button">Search</button>
+        <input
+          type="text"
+          placeholder="Search for medicines..."
+          className="search-input"
+          value={searchInput}
+          onChange={handleInputChange}
+          list="medicine-suggestions"
+        />
+        <datalist id="medicine-suggestions">
+          {filteredSuggestions.map((medicine, index) => (
+            <option key={index} value={medicine} />
+          ))}
+        </datalist>
+        <button className="search-button" onClick={handleSearch}>Search</button>
       </div>
 
+      <div className="comparison-image">
+        <h2 style={{ color: 'black' }}>Here’s a quick comparison</h2>
+        <img
+          src={process.env.PUBLIC_URL + '/Comparison.png'}
+          alt="Comparison Image"
+          className="comparison-image"
+        />
+      </div>
 
       {/* Video section */}
       <div className="video-section">
-        <h2>Explore Our Services</h2>
-        <video controls width="480" height="400">
+        <h2>Listen to our GxPerts talking about Generic Medicines</h2>
+        <video controls width="420" height="250">
           <source
             src={process.env.PUBLIC_URL + '/Video_20231211_202116_303_1.mp4'} // Update the video file name
             type="video/mp4"
@@ -64,21 +104,18 @@ const Home = () => {
       {/* Section on why generic medicines */}
       <div id="why-generic-section" className="why-generic-section">
         <h2>Why Choose Generic Medicines?</h2>
-        <p>
-          Generic medicines are cost-effective alternatives to brand-name drugs. They contain the
-          same active ingredients, ensuring the same quality and effectiveness.
-        </p>
-        <p>
-          Doctors often recommend generic medicines as they provide the same therapeutic benefits
-          at a lower cost, making healthcare more accessible.
-        </p>
+        <img
+          src={process.env.PUBLIC_URL + '/Frame 54933 (1).png'}
+          alt="generic section image"
+          className="why-generic-section-image"
+        />
         {/* Add more information as needed */}
       </div>
 
-      <div className="home-buttons">
+      {/* <div className="home-buttons">
         <button className="dummy-button">Browse Categories</button>
         <button className="dummy-button">Special Offers</button>
-      </div>
+      </div> */}
     </div>
   );
 };
